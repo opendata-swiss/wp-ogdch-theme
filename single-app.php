91953021974ffe301efdd8287091046c8eb5e02a
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-08-31 10:54:16 +0200

    add the loop to single template

--- single-app.php
+++ single-app.php
@@ -7,9 +7,13 @@
 			<a href="<?php esc_attr_e( get_post_type_archive_link( 'app' ) ); ?>">< Back to App-List</a>
 		</div>
 	</div>
-	<div class="row">
-		<?php get_template_part( 'partials/app', 'entry' ); ?>
-	</div>
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post();?>
+			<div class="row">
+				<?php get_template_part( 'partials/app', 'entry' ); ?>
+			</div>
+		<?php endwhile; ?>
+	<?php endif; ?>
 </div>
 
 <?php get_footer(); ?>
