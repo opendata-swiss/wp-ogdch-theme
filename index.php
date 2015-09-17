196853fdb858fc4d9d1f6aa33e9bf923c7531a6d
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-09-17 11:43:05 +0200

    implement data categories

--- index.php
+++ index.php
@@ -18,40 +18,61 @@
 </header>
 
 <div id="content">
-	<div id="explore">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-9">
-					<h2>What you can find</h2>
-				</div>
-				<div class="col-lg-3 text-right">
-					<form class="form-inline" role="search">
-						<div class="form-group">
-							<input type="search" class="form-control" id="s" name="s" placeholder="<?php esc_attr_e( 'Search opendata.swiss', 'ogdch' ); ?>">
-						</div>
-						<button type="submit" class="btn btn-default" aria-label="<?php esc_attr_e( 'Search', 'ogdch' ); ?>"><i class="fa fa-search" aria-hidden="true"></i></button>
-					</form>
-				</div>
-
+	<!-- Explore -->
+	<section id="explore" class="container">
+		<div class="row">
+			<div class="col-lg-9">
+				<h2>What you can find</h2>
 			</div>
-
-			<div class="row">
-
-				<?php if ( have_posts() ) : ?>
-					<?php while ( have_posts() ) : the_post();?>
-						<div class="col-lg-12">
-							<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-							<?php the_content(); ?>
-							<hr>
-						</div>
-					<?php endwhile; ?>
-
-				<?php else : ?>
-					<?php echo '<p>' . esc_html( __( 'Nothing found!', 'ogdch' ) ) . '</p>'; ?>
-				<?php endif; ?>
+			<div class="col-lg-3 text-right">
+				<form class="form-inline h2-vertical-center" role="search">
+					<div class="form-group">
+						<input type="search" class="form-control" id="s" name="s" placeholder="<?php esc_attr_e( 'Search opendata.swiss', 'ogdch' ); ?>">
+					</div>
+					<button type="submit" class="btn btn-default" aria-label="<?php esc_attr_e( 'Search', 'ogdch' ); ?>"><i class="fa fa-search" aria-hidden="true"></i></button>
+				</form>
 			</div>
 		</div>
-	</div>
+
+		<div class="row">
+			<div class="col-lg-12">
+				<h3>Data Categories</h3>
+			</div>
+		</div>
+
+		<div class="row">
+			<?php
+			$args = array(
+				'post_type' => 'ckan-local-group',
+				'posts_per_page' => -1,
+			);
+			$groups_query = new WP_Query( $args );
+			?>
+			<?php while ( $groups_query->have_posts() ) : $groups_query->the_post(); ?>
+				<div class="col-lg-3">
+					<h4><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a> 12</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</div>
+			<?php endwhile; wp_reset_query(); ?>
+		</div>
+	</section>
+
+
+	<!-- How -->
+	<section id="how" class="container">
+		<div class="row">
+			<div class="col-lg-9">
+				<h2>How you can build</h2>
+			</div>
+			<div class="col-lg-3 text-right">
+				<a class="btn btn-primary h2-vertical-center" href="#" role="button">Read our FAQ</a>
+			</div>
+		</div>
+
+		<div class="row">
+
+		</div>
+	</section>
 </div>
 
 
