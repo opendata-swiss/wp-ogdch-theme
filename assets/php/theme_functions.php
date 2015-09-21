f21510e2a63f03698a8cf150bd03237b4cbb50f1
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-09-21 15:54:32 +0200

    implement function to get meta for current locale / add active class to active nav item

--- assets/php/theme_functions.php
+++ assets/php/theme_functions.php
@@ -3,3 +3,31 @@
 // =======================================================================//
 // Theme Functions
 // =======================================================================//
+
+function get_localized_meta( $post_id, $meta_name ) {
+	global $language_priority;
+
+	// try to get meta in current language
+	$localized_meta = get_post_meta( $post_id, $meta_name . pll_current_language(), true );
+	if( !empty( $localized_meta) ) {
+		return $localized_meta;
+	}
+
+	// use other languages as fallback
+	foreach( $language_priority as $lang ) {
+		$localized_meta = get_post_meta( $post_id, $meta_name . $lang, true );
+		if( !empty( $localized_meta ) ) {
+			return $localized_meta;
+		}
+	}
+	return false;
+}
+
+
+add_filter('nav_menu_css_class' , 'add_class_to_current_item' , 10 , 2);
+function add_class_to_current_item($classes, $item){
+	if( in_array('current-menu-item', $classes) ){
+		$classes[] = 'active ';
+	}
+	return $classes;
+}
