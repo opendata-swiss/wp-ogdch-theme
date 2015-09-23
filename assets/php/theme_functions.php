d678754645a0769bb5ea289227cc68656ca59a4d
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-09-23 16:01:22 +0200

    Fix phpcs in theme_functions

--- assets/php/theme_functions.php
+++ assets/php/theme_functions.php
@@ -1,50 +1,82 @@
 <?php
 
-// =======================================================================//
-// Theme Functions
-// =======================================================================//
-
+/**
+ * Get localized metafield value.
+ *
+ * @param int    $post_id   The ID of the post.
+ * @param string $meta_name Name of the metafield to return.
+ *
+ * @return bool
+ */
 function get_localized_meta( $post_id, $meta_name ) {
 	global $language_priority;
 
 	// try to get meta in current language
 	$localized_meta = get_post_meta( $post_id, $meta_name . pll_current_language(), true );
-	if( !empty( $localized_meta) ) {
+	if ( ! empty( $localized_meta ) ) {
 		return $localized_meta;
 	}
 
 	// use other languages as fallback
-	foreach( $language_priority as $lang ) {
+	foreach ( $language_priority as $lang ) {
 		$localized_meta = get_post_meta( $post_id, $meta_name . $lang, true );
-		if( !empty( $localized_meta ) ) {
+		if ( ! empty( $localized_meta ) ) {
 			return $localized_meta;
 		}
 	}
 	return false;
 }
 
-
-add_filter('nav_menu_css_class' , 'add_class_to_current_item' , 10 , 2);
-function add_class_to_current_item($classes, $item){
-	if( in_array('current-menu-item', $classes) ){
+/**
+ * Add class to current item.
+ *
+ * @param array $classes Array of CSS classes.
+ * @param mixed $item    Item.
+ *
+ * @return array
+ */
+function add_class_to_current_item( $classes, $item ) {
+	if ( in_array( 'current-menu-item', $classes ) ) {
 		$classes[] = 'active ';
 	}
 	return $classes;
 }
+add_filter( 'nav_menu_css_class' , 'add_class_to_current_item' , 10 , 2 );
 
-function get_page_link_by_slug($slug) {
+/**
+ * Get relative link to page
+ *
+ * @param string $slug The slug to link to.
+ *
+ * @return string
+ */
+function get_page_link_by_slug( $slug ) {
 	return '/' . pll_current_language() . '/' . $slug;
 }
 
+/**
+ * Number of post of specified type
+ *
+ * @param string $post_type Name of post type.
+ *
+ * @return int|mixed
+ */
 function get_localized_post_count( $post_type ) {
+	// @codingStandardsIgnoreStart
 	$args = array(
 		'post_type' => $post_type,
 		'posts_per_page' => -1,
 	);
 	$posts = new WP_Query( $args );
+	// @codingStandardsIgnoreEnd
 	return $posts->found_posts;
 }
 
+/**
+ * Get dataset count from CKAN
+ *
+ * @return int
+ */
 function get_dataset_count() {
 	$transient_name = 'ogdch_dataset_count';
 	if ( false === ( $dataset_count = get_transient( $transient_name ) ) ) {
