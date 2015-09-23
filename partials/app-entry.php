0d998019fc91b1f22d13aa74c92562f815fc42e7
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-09-23 15:04:44 +0200

    FIx phpcs in app-entry

--- partials/app-entry.php
+++ partials/app-entry.php
@@ -5,7 +5,7 @@ $author_email = get_post_meta( get_the_ID(), '_app-showcase-app_author_email', t
 $version      = get_post_meta( get_the_ID(), '_app-showcase-app_version', true );
 $related_datasets = get_post_meta( get_the_ID(), '_app-showcase-app_relations', true );
 $icon_attributes = wp_get_attachment_image_src( $icon_id, 'full' );
-if( $icon_attributes ) {
+if ( $icon_attributes ) {
 	$icon_src = $icon_attributes[0];
 } else {
 	$icon_src = '/content/themes/wp-ogdch-theme/assets/images/no_image_available.png';
@@ -14,27 +14,27 @@ if( $icon_attributes ) {
 
 <div class="row">
 	<div class="col-sm-3 h2-vertical-center">
-		<img src="<?php echo $icon_src; ?>" class="img-responsive" />
+		<img src="<?php esc_attr_e( $icon_src ); ?>" class="img-responsive" />
 	</div>
 	<div class="col-sm-9">
 		<?php
 		echo '<h2>' . esc_html( get_the_title() ) . '</h2>';
 		echo '<p class="small">';
 		echo 'Author: <a href="mailto:' . esc_attr( $author_email ) . '">' . esc_attr( $author_name ) . '</a>';
-		if( !empty( $version ) ) {
+		if ( ! empty( $version ) ) {
 			echo ' | Version: ' . esc_attr( $version );
 		}
 		echo '</p>';
 		echo '<p>' . the_content() . '</p>';
-		if( !empty($related_datasets) ) {
-			echo '<div class="collapse" id="collapsed-related-' . get_the_ID() . '">';
+		if ( ! empty( $related_datasets ) ) {
+			echo '<div class="collapse" id="collapsed-related-' . esc_attr( get_the_ID() ) . '">';
 			echo '<ul>';
-			foreach($related_datasets as $dataset) {
-				echo '<li>' . $dataset['dataset_id'] . '</li>';
+			foreach ( $related_datasets as $dataset ) {
+				echo '<li>' . esc_html( $dataset['dataset_id'] ) . '</li>';
 			}
 			echo '</ul>';
 			echo '</div>';
-			echo '<p class="small"><a id="collapsed-related-' . get_the_ID() . '-link" data-toggle="collapse" href="#collapsed-related-' . get_the_ID() . '" aria-expanded="false" aria-controls="collapsed-related-' . get_the_ID() . '">Show related datasets</a></p>';
+			echo '<p class="small"><a id="collapsed-related-' . esc_attr( get_the_ID() ) . '-link" data-toggle="collapse" href="#collapsed-related-' . esc_attr( get_the_ID() ) . '" aria-expanded="false" aria-controls="collapsed-related-' . esc_attr( get_the_ID() ) . '">Show related datasets</a></p>';
 		}
 
 		?>
