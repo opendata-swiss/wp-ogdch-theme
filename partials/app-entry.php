45b115ea97b2e0e5c249770d883a424377376afc
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-10-28 14:48:49 +0100

    Fix related dataset title

--- partials/app-entry.php
+++ partials/app-entry.php
@@ -1,10 +1,10 @@
 <?php
-$icon_id      = get_post_meta( get_the_ID(), '_app-showcase-app_icon_id', true );
-$author_name  = get_post_meta( get_the_ID(), '_app-showcase-app_author_name', true );
-$author_email = get_post_meta( get_the_ID(), '_app-showcase-app_author_email', true );
-$url          = get_post_meta( get_the_ID(), '_app-showcase-app_url', true );
+$icon_id          = get_post_meta( get_the_ID(), '_app-showcase-app_icon_id', true );
+$author_name      = get_post_meta( get_the_ID(), '_app-showcase-app_author_name', true );
+$author_email     = get_post_meta( get_the_ID(), '_app-showcase-app_author_email', true );
+$url              = get_post_meta( get_the_ID(), '_app-showcase-app_url', true );
 $related_datasets = get_post_meta( get_the_ID(), '_app-showcase-app_relations', true );
-$icon_attributes = wp_get_attachment_image_src( $icon_id, 'full' );
+$icon_attributes  = wp_get_attachment_image_src( $icon_id, 'full' );
 if ( $icon_attributes ) {
 	$icon_src = $icon_attributes[0];
 } else {
@@ -28,9 +28,10 @@ if ( $icon_attributes ) {
 				esc_attr_e( 'Please activate wp-ckan-backend plugin', 'ogdch' );
 			} else {
 				echo '<ul>';
-				foreach ( $related_datasets as $dataset ) {
-					$title = Ckan_Backend_Helper::get_dataset_title( $dataset['dataset_id'] );
-					echo '<li><a href="' . esc_attr( get_page_link_by_slug( 'dataset/' . $dataset['dataset_id'] ) ) . '">' . esc_attr( $title ) . '</a></li>';
+				foreach ( $related_datasets as $related_dataset ) {
+					$dataset = Ckan_Backend_Helper::get_dataset( $related_dataset['dataset_id'] );
+					$dataset_title = Ckan_Backend_Helper::get_dataset_title( $related_dataset['dataset_id'] );
+					echo '<li><a href="' . esc_attr( get_page_link_by_slug( 'dataset/' . $dataset['name'] ) ) . '">' . esc_attr( $dataset_title ) . '</a></li>';
 				}
 				echo '</ul>';
 			}
