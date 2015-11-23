5527309d87610389ca9d674a9d755ceac97be6af
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-11-23 18:14:51 +0100

    fix phpcs

--- footer.php
+++ footer.php
@@ -113,14 +113,14 @@ $dataset_count = get_dataset_count();
 	_paq.push(['trackPageView']);
 	_paq.push(['enableLinkTracking']);
 	(function() {
-		var u="//<?php echo PIWIK_URL; ?>/";
+		var u="//<?php echo esc_attr( PIWIK_URL ); ?>/";
 		_paq.push(['setTrackerUrl', u+'piwik.php']);
-		_paq.push(['setSiteId', <?php echo PIWIK_SITEID; ?>]);
+		_paq.push(['setSiteId', <?php echo intval( PIWIK_SITEID ); ?>]);
 		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 		g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 	})();
 </script>
-<noscript><p><img src="//<?php echo PIWIK_URL; ?>/piwik.php?idsite=<?php echo PIWIK_SITEID; ?>" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="//<?php echo esc_attr( PIWIK_URL ); ?>/piwik.php?idsite=<?php echo intval( PIWIK_SITEID ); ?>" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
 
 <?php wp_footer(); ?>
