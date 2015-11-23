59707f1132c1b7a71259f8037f55f7686decc89b
Juerg Hunziker <juerg.hunziker@liip.ch>
2015-11-23 08:35:36 +0100

    fix cleanup script

--- assets/php/cleanup.php
+++ assets/php/cleanup.php
@@ -13,15 +13,15 @@ function head_cleanup() {
 	remove_action( 'wp_head', 'rsd_link' );
 	remove_action( 'wp_head', 'wlwmanifest_link' );
 	remove_action( 'wp_head', 'index_rel_link' );
-	remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
-	remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
-	remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
+	remove_action( 'wp_head', 'parent_post_rel_link', 10 );
+	remove_action( 'wp_head', 'start_post_rel_link', 10 );
+	remove_action( 'wp_head', 'adjacent_posts_rel_link', 10 );
 	remove_action( 'wp_head', 'wp_generator' );
 	remove_action( 'wp_head', array( $sitepress, 'meta_generator_tag' ) );
-	remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
-	remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+	remove_action( 'wp_head', 'start_post_rel_link', 10 );
+	remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10 );
 	remove_action( 'wp_head', 'rel_canonical' );
-	remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+	remove_action( 'wp_head', 'wp_shortlink_wp_head', 10 );
 
 	remove_action( 'wp_head', array(
 		$wp_widget_factory->widgets['WP_Widget_Recent_Comments'],
@@ -34,28 +34,7 @@ function head_cleanup() {
 add_action( 'init', 'head_cleanup' );
 
 /**
- * Remove Widgets
- *
- * @return void
- */
-function unregister_default_wp_widgets() {
-	unregister_widget( 'WP_Widget_Pages' );
-	unregister_widget( 'WP_Widget_Calendar' );
-	unregister_widget( 'WP_Widget_Archives' );
-	unregister_widget( 'WP_Widget_Links' );
-	unregister_widget( 'WP_Widget_Meta' );
-	unregister_widget( 'WP_Widget_Search' );
-	unregister_widget( 'WP_Widget_Text' );
-	unregister_widget( 'WP_Widget_Categories' );
-	unregister_widget( 'WP_Widget_Recent_Posts' );
-	unregister_widget( 'WP_Widget_RSS' );
-	unregister_widget( 'WP_Widget_Tag_Cloud' );
-}
-
-add_action( 'widgets_init', 'unregister_default_wp_widgets' );
-
-/**
- * Eemove Dashboard Widgets
+ * Remove Dashboard Widgets
  *
  * @return void
  */
