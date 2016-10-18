554f4189494e8da4e1b78e149d63bb04d40d4e84
Juerg Hunziker <juerg.hunziker@liip.ch>
2016-10-18 09:03:23 +0200

    add responsive embed filter

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ $frontend_includes = array(
 	'assets/php/scripts.php',
 	'assets/php/theme_functions.php',
 	'assets/php/multilingual_ninjaforms.php',
+	'assets/php/bootstrap_embed_responsive.php',
 	'assets/external/wp-bootstrap-navwalker/wp_bootstrap_navwalker.php',
 );
 
